fa19ea29d3eda3260eb76eeabf69a9ee68ce2aee
Greg Hosking <hoskinggregory@gmail.com>
2024-04-08 19:18:40 -0400

    Add Musixmatch utility functions

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+const MusixmatchError = require("./errors");
+const musixmatchFetch = require("./fetch");
+const MusixmatchValidator = require("./validator");
+
+module.exports = {
+    MusixmatchError,
+    musixmatchFetch,
+    MusixmatchValidator,
+};
