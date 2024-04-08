0c33c7d9a41d1f5484faf252753a2096a61ed573
Greg Hosking <hoskinggregory@gmail.com>
2024-04-08 19:21:36 -0400

    Remove ERRORS constant from index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,9 @@
 const CHARTS = require("./charts");
 const COUNTRIES = require("./countries");
 const DEFAULTS = require("./defaults");
-const ERRORS = require("./errors");
 
 module.exports = {
     CHARTS,
     COUNTRIES,
     DEFAULTS,
-    ERRORS,
 };
