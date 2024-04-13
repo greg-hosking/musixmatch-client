add8d25b1e6ecbfca4d51051d32650aa4dad35b2
Greg Hosking <hoskinggregory@gmail.com>
2024-04-13 01:13:35 -0400

    Delete unnecessary lyrics controller

--- src/controllers/lyrics.js
+++
@@ -1,9 +0,0 @@
-const { Lyrics } = require("../models");
-
-class LyricsController {
-    static async getLyricsByTrackId({ apiKey, id, commonTrackId } = {}) {
-        // TO DO
-    }
-}
-
-module.exports = LyricsController;
