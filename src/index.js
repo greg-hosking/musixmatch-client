9b8cdcf4bebe4085ff9e3f49c39d78f5ef822745
Greg Hosking <hoskinggregory@gmail.com>
2024-04-19 01:27:17 -0400

    Update index.js to only export MusixmatchClient

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 const MusixmatchClient = require("./client");
-const { Album, Artist, Genre, Lyrics, Track } = require("./entities");
 
-module.exports = { MusixmatchClient, Album, Artist, Genre, Lyrics, Track };
+module.exports = MusixmatchClient;
