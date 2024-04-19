4cea8b4fb6df25f46262d102a03fe9048059698f
Greg Hosking <hoskinggregory@gmail.com>
2024-04-19 01:39:50 -0400

    Remove unused method and log params in errors

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,26 +2,11 @@ const { CHARTS, COUNTRIES } = require("../constants");
 const MusixmatchError = require("./errors");
 
 class MusixmatchValidator {
-    static validateApiKey(apiKey) {
-        if (!apiKey) {
-            throw new MusixmatchError(
-                400,
-                "Missing required parameter: apiKey"
-            );
-        }
-        if (typeof apiKey !== "string" || !apiKey.trim()) {
-            throw new MusixmatchError(
-                400,
-                "Invalid parameter: apiKey. The value must be a non-empty string."
-            );
-        }
-    }
-
     static validateChart(chart) {
         if (!Object.values(CHARTS).includes(chart)) {
             throw new MusixmatchError(
                 400,
-                "Invalid chart. Check MusixmatchClient.CHARTS for valid charts."
+                `Invalid chart: ${chart}. Check MusixmatchClient.CHARTS for valid charts.`
             );
         }
     }
@@ -30,7 +15,7 @@ class MusixmatchValidator {
         if (!Object.values(COUNTRIES).includes(country)) {
             throw new MusixmatchError(
                 400,
-                "Invalid country. Check MusixmatchClient.COUNTRIES for valid countries."
+                `Invalid country: ${country}. Check MusixmatchClient.COUNTRIES for valid countries.`
             );
         }
     }
@@ -39,7 +24,7 @@ class MusixmatchValidator {
         if (!Number.isInteger(page) || page < 1) {
             throw new MusixmatchError(
                 400,
-                "Invalid page. The page must be a positive integer."
+                `Invalid page: ${page}. The page must be a positive integer.`
             );
         }
     }
@@ -48,7 +33,7 @@ class MusixmatchValidator {
         if (!Number.isInteger(pageSize) || pageSize < 1 || pageSize > 100) {
             throw new MusixmatchError(
                 400,
-                "Invalid page size. The page size must be an integer between 1 and 100."
+                `Invalid page size: ${pageSize}. The page size must be an integer between 1 and 100.`
             );
         }
     }
